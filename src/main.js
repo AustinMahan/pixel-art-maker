863ba95e02bb25177784abe7a32e0cc988e1b40f
AustinMahan <amahan0424@gmail.com>
2016-07-21 11:43:36 -0600

    deploy to branch

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
   for (var i = 0; i < 2528; i++) {
-    $('#canvas').append('<div class="emptyBox"></div>')
+    $('#canvas').append('<div class="emptyBox"></div>');
   };
 
   var currentColor;
 
   $('#palete > div').click(function () {
-    currentColor = $(this).css('background-color')
+    currentColor = $(this).css('background-color');
     console.log(currentColor);
     $('#currentColor').css('background-color', currentColor);
-  })
+  });
 
   var isDown = false;
 
   $('#canvas > div').mousedown(function() {
       isDown = true;
-    })
+    });
   $('#canvas > div').mouseup(function() {
       isDown = false;
     });
 
   $("#canvas > div").mouseover(function() {
-    if(isDown) {
-        $(this).css('background-color', currentColor)
-     }
+      if(isDown) {
+        $(this).css('background-color', currentColor);
+      }
     });
 
   $('#canvas > div').click(function() {
-       $(this).css('background-color', currentColor)
-    });
+    $(this).css('background-color', currentColor);
+  });
 
   // $('#canvas > div').hover(function () {
   //   $(this).mousedown(function () {
